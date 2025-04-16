e17f11e9981b25d8efce8b4e37c709d3653b22b4
alpha <meet567@gmail.com>
2025-04-16 15:26:30 +0800

    測試git追蹤

---
+++ testgit.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 16 15:21:06 2025
+
+@author: USER
+"""
+
+print('test git')
---
+++ testgit2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 16 15:22:55 2025
+
+@author: USER
+"""
+
+print('5*2=',5*2)
