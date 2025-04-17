e65297d56fb1ae9b55f537d8cfa042444b91ccc2
alpha <meet567@gmail.com>
2025-04-17 09:33:29 +0800

    新增筆記

---
+++ test4.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 17 09:19:55 2025
+
+@author: USER
+"""
+
+print('hello~! world4')
+print('vi test')
+
---
+++ test5.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 17 09:20:17 2025
+
+@author: USER
+"""
+
+print('hello~! 555')
